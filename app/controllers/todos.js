f6efa6fc0d863b36fddcc37033ed3c497e1d382b
Andy Brown <brownie3003@gmail.com>
2014-12-28 12:09:25 +0100

    Add the finishing touches to the repo

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -16,6 +16,39 @@ export default Ember.ArrayController.extend({
  
             // Save the new model
             todo.save();
+        },
+        clearCompleted: function() {
+            var completed = this.filterBy('isCompleted', true);
+            completed.invoke('deleteRecord');
+            completed.invoke('save');
         }
-    }
+    },
+    
+    remaining: function() {
+        return this.filterBy('isCompleted', false).get('length');
+    }.property('@each.isCompleted'),
+ 
+    inflection: function() {
+        var remaining = this.get('remaining');
+        return (remaining === 1) ? 'item' : 'items';
+    }.property('remaining'),
+
+    hasCompleted: function() {
+        return this.get('completed') > 0;
+    }.property('completed'),
+     
+    completed: function() {
+        return this.filterBy('isCompleted', true).get('length');
+    }.property('@each.isCompleted'),
+
+    allAreDone: function(key, value) {
+        console.log(key + ": " + value);
+        if (value === undefined) {
+            return this.get('length') > 0 && this.isEvery('isCompleted', true);
+        } else {
+            this.setEach('isCompleted', value);
+            this.invoke('save');
+            return value;
+        }
+    }.property('@each.isCompleted')
 });
