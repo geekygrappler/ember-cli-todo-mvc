d7aa954761f374784e3f82a531263ed31c0367f5
Andy Brown <brownie3003@gmail.com>
2015-01-01 21:19:07 +0000

    rename to server

---
+++ server/api/controllers/TodoController.js
@@ -0,0 +1,11 @@
+/**
+ * TodoController
+ *
+ * @description :: Server-side logic for managing todoes
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
